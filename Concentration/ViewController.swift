ad7ddf4bb3903d939802fdc04d99a02c6da54ec0
Jackson Melcher <jacksonmelcher@protonmail.com>
2017-11-16 15:04:50 -0800

    Added flip count variable and label.

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -10,12 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var flipCount = 0 {
+        didSet {
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
+    }
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
     
     @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
         flipCard(withEmoji: "👻", on: sender)
     }
     
     @IBAction func touchSecondCard(_ sender: UIButton) {
+        flipCount += 1
+        
         flipCard(withEmoji: "🧛‍♀️", on: sender)
     }
     
