93fb20350f6a08c4e697a0bd0fe9e43a86ef810f
Jackson Melcher <jacksonmelcher@protonmail.com>
2017-11-16 14:55:37 -0800

    Added two buttons that flip when clicked.

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -11,6 +11,25 @@ import UIKit
 class ViewController: UIViewController {
 
     
-
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCard(withEmoji: "👻", on: sender)
+    }
+    
+    @IBAction func touchSecondCard(_ sender: UIButton) {
+        flipCard(withEmoji: "🧛‍♀️", on: sender)
+    }
+    
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+       
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5781051517, blue: 0, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
+        }
+    }
+    
 }
 
