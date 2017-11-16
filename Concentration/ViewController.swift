c23f20c0c24e617022b99ee7f560c7d1bdf1a80d
Jackson Melcher <jacksonmelcher@protonmail.com>
2017-11-16 14:33:10 -0800

    Created new xCode project. Added button.

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Jackson Melcher on 11/16/17.
+//  Copyright © 2017 JAX. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+
+}
+
