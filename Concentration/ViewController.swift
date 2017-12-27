f4cee06064538c2e258481dbf9bd4e48102b6954
Jackson Melcher <jacksonmelcher@protonmail.com>
2017-12-27 12:16:43 -0800

    Added a dictionary and random functionto handle emoji allocation to each card.

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -55,8 +55,16 @@ class ViewController: UIViewController {
     
     
     var emojiChoices = ["👻","🧛‍♀️","🐭","🐛", "🌼", "🍄", "🦏", "🐙"]
+    
+    var emoji = [Int:String]()
+    
     func emoji(for card: Card) -> String {
-        return "?"
+        if emoji[card.identifier] == nil, emojiChoices.count > 0 {
+            let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.count)))
+            emoji[card.identifier] = emojiChoices.remove(at: randomIndex)
+        }
+        
+        return emoji[card.identifier] ?? "?"
     }
     
     
